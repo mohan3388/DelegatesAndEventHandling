9fcbf2f5d691380f71c5269dc2636af3064cc87e
mohan338 <sahumohan338@gmail.com>
2022-05-18 17:00:43 +0530

    [mohan]add: Delegate Addition of two number

---
+++ SingleCastDelegates/Delegate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleCastDelegate
+{
+    internal class Delegate
+    {
+        public delegate int Operation(int x, int y);
+
+        public static int Addition(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ SingleCastDelegates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using SingleCastDelegate;
+using static SingleCastDelegate.Delegate;
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        Operation obj = new Operation(Addition);
+        Console.WriteLine("Addition of two number using delegate is:" + obj(51, 10));
+    }
+}
